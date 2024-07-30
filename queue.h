4a7d469afa8373c4ccc9310cfe07ef31494be543
Elliot Skogsberg <elliot.skogsberg@gmail.com>
2024-07-31 01:46:53 +0200

    inital commit

---
+++ queue.h
@@ -0,0 +1,93 @@
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+// FIXME: Enqueue/dequeue fail when writing/reading at the end of the buffer. Needs to wrap around in this edge case.
+
+typedef struct Queue {
+    void*   data;       // Pointer to the start of data.
+    size_t  size;       // Current size of the queue in bytes.
+    size_t  capacity;   // Total capacity of the queue in bytes.
+    size_t  head;       // Byte offset for the head of the queue.
+    size_t  tail;       // Byte offset for the tail of the queue.
+    size_t  count;      // Number of items in the queue, +1 per enqueue and -1 per dequeue.
+    bool    dynamic;    // Whether the queue will dynamically reallocate to ensure enough capacity.
+} Queue;
+
+// Create queue with `capacity` bytes total capacity. If `dynamic` is set to true, the queue will reallocate on overflow.
+Queue queue_create(size_t capacity, bool dynamic);
+
+// Destroy `queue`, freeing all resources.
+void queue_destroy(Queue* queue);
+
+// Ensure `queue` has at least `size` bytes of capacity.
+void queue_ensure(Queue* queue, size_t size);
+
+// Add `size` bytes of `data` to the tail of `queue`.
+void queue_enqueue(Queue* queue, size_t size, const void* data);
+
+// Remove `size` bytes from the head of `queue` and move them into `data`.
+void queue_dequeue(Queue* queue, size_t size, void* data);
+
+#ifdef QUEUE_IMPLEMENTATION
+
+Queue queue_create(size_t capacity, bool dynamic) {
+    return (Queue) {
+        .data = malloc(capacity),
+        .capacity = capacity,
+        .dynamic = dynamic,
+        .count = 0,
+        .size = 0,
+        .head = 0,
+        .tail = 0,
+    };
+}
+
+void queue_destroy(Queue* queue) {
+    free(queue->data);
+    queue->data = NULL;
+    queue->dynamic = false;
+    queue->capacity = 0;
+    queue->size = 0;
+    queue->count = 0;
+    queue->head = 0;
+    queue->tail = 0;
+}
+
+void queue_ensure(Queue* queue, size_t size) {
+    while (queue->capacity < size) {
+        queue->capacity *= 2;
+    }
+    queue->data = realloc(queue->data, queue->capacity);
+}
+
+void queue_enqueue(Queue* queue, size_t size, const void* data) {
+    if (queue->dynamic && queue->size + size >= queue->capacity) {
+        queue_ensure(queue, queue->size + size);
+    }
+    memmove((uint8_t*)queue->data + queue->tail, data, size);
+    queue->tail += size;
+    if (queue->tail >= queue->capacity) {
+        queue->tail -= queue->capacity;
+    }
+    queue->size += size;
+    queue->count++;
+}
+
+void queue_dequeue(Queue* queue, size_t size, void* data) {
+    memmove(data, (uint8_t*)queue->data + queue->head, size);
+    queue->head += size;
+    if (queue->head >= queue->capacity) {
+        queue->head -= queue->capacity;
+    }
+    queue->size -= size;
+    queue->count--;
+}
+
+#endif
+
+#endif
---
+++ stack.h
@@ -0,0 +1,77 @@
+#ifndef STACK_H
+#define STACK_H
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef struct Stack {
+    void*   data;       // Pointer to the start of data.
+    size_t  size;       // Current size of the stack in bytes.
+    size_t  capacity;   // Total capacity of the queue in bytes.
+    size_t  count;      // Number of items in the stack, +1 per push and -1 per pop. 
+    bool    dynamic;    // Whether the stack will dynamically reallocate to ensure enough capacity.
+} Stack;
+
+// Create stack with `capacity` bytes total capacity. If `dynamic` is set to true, the stack will reallocate on overflow.
+Stack stack_create(size_t capacity, bool dynamic);
+
+// Destroy `stack`, freeing all resources.
+void stack_destroy(Stack* stack);
+
+// Ensure `stack` has at least `size` bytes of capacity.
+void stack_ensure(Stack* stack, size_t size);
+
+// Add `size` bytes of `data` to the top of `stack`.
+void stack_push(Stack* stack, size_t size, const void* data);
+
+// Remove `size` bytes from the top of `stack` and move them into `data`.
+void stack_pop(Stack* stack, size_t size, void* data);
+
+#ifdef STACK_IMPLEMENTATION
+
+Stack stack_create(size_t capacity, bool dynamic) {
+    return (Stack) {
+        .data = malloc(capacity),
+        .capacity = capacity,
+        .dynamic = dynamic,
+        .size = 0,
+        .count = 0,
+    };
+}
+
+void stack_destroy(Stack* stack) {
+    free(stack->data);
+    stack->data = NULL;
+    stack->count = 0;
+    stack->size = 0;
+    stack->dynamic = false;
+    stack->capacity = 0;
+}
+
+void stack_ensure(Stack* stack, size_t size) {
+    while (stack->capacity < size) {
+        stack->capacity *= 2;
+    }
+    stack->data = realloc(stack->data, stack->capacity);
+}
+
+void stack_push(Stack* stack, size_t size, const void* data) {
+    if (stack->dynamic && stack->size + size >= stack->capacity) {
+        stack_ensure(stack, stack->size + size);
+    }
+    memmove((uint8_t*)stack->data + stack->size, data, size);
+    stack->size += size;
+    stack->count++;
+}
+
+void stack_pop(Stack* stack, size_t size, void* data) {
+    stack->size -= size;
+    memmove(data, (uint8_t*)stack->data + stack->size, size);
+    stack->count--;
+}
+
+#endif
+
+#endif
